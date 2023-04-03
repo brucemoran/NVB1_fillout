04f127f058822058d4d8fa75bcecd9a788f7e971
brucemoran <bruce01campus@gmail.com>
2023-04-03 12:18:54 +0100

    Revis

---
+++ inst/app/app.R
@@ -0,0 +1,19 @@
+shiny::fluidPage(
+  shinyjs::useShinyjs(),
+  shiny::headerPanel("SI Garda Vetting NVB1 Filling Application"),
+  shiny::sidebarLayout(
+    shiny::sidebarPanel(width=12,
+                        shiny::splitLayout(
+                          shiny::fileInput(
+                            inputId = "FILENAMES",
+                            label = NULL,
+                            multiple = FALSE,
+                            accept = c(".csv"),
+                            buttonLabel = "Import CSV...",
+                            placeholder = NULL),
+                          shiny::actionButton("manual", "Enter Data Manually")
+                        ),
+    ),
+    shiny::mainPanel()
+  )
+)
--- inst/app/server.R
+++ inst/app/server.R
@@ -1,18 +1,6 @@
-#! R
-
-##fill in the supplied NVB1 form programmatically
-##allowed input is by user input (modal)
-##and by upload of CSV
-
 function(input, output, session) {
-
-  ##create reactive to store values from CSV and parse CSV input
   vals <- shiny::reactiveValues(CSV = NULL, OUT = NULL)
-
   obsev_FILENAMES(input, vals)
-
   obsev_manual(input, vals)
-
   obsev_go_fill_pdf(input, vals)
-
 }
